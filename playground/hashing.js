53232fa3f4fe0c5a20493e2b626261ff00fd4fb2
suhtite <suhtite123@gmail.com>
2017-08-03 09:11:14 +0630

    hash user password and saveit

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,24 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+
+var password ='123abc!';
+
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt,(err,hash)=>{
+        console.log(hash);
+    });
+});
+
+
+var hashedPassword='$2a$10$P1vJxw2mh8ASbV9A.MnpC.d2Dft78emm3mgoHl4ks/odIadsYultW';
+
+bcrypt.compare('123!',hashedPassword,(err,result)=>{
+    console.log(result);
+})
+
+/*
 
 var data ={
     id:10
@@ -12,6 +30,7 @@ console.log(token);
 
 var decoded = jwt.verify(token ,'123abc');
 console.log('Decoded' , decoded);
+*/
 
 /*
 var message = 'I am user number 3';
