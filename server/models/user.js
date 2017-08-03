a73f14340d2baa7a4a544314d994f6dc6d962257
suhtite <suhtite123@gmail.com>
2017-08-03 15:26:33 +0630

    Add DELETE Users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -54,12 +54,21 @@ UserSchema.methods.generateAuthToken = function () {
     return user.save().then(()=> {
         return token;
     });
-    /*.then((token)=>{
-
-     })*/
-
 
 };
+
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull :{
+            tokens :{
+                token 
+            }
+        }
+    })
+};
+
 
 UserSchema.statics.findByToken = function (token) {
     var User = this;
